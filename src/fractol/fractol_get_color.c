f811184f8227ccf9fe3a0ba38e31680dec7027fb
맹주영 <mjy9088@naver.com>
2022-08-31 21:32:15 +0900

    add rainbow color

--- src/fractol/fractol_get_color.c
+++ src/fractol/fractol_get_color.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 01:58:08 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/08/31 21:05:06 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/08/31 21:30:28 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,43 @@
 #include "fractol_mandelbrot.h"
 #include "ft_math_complex.h"
 
-#define LIMIT 20
+#define LIMIT 42
+#define STEPS 3
+
+static const t_fractol_pixel	g_black = {0.0L, 0.0L, 0.0L};
+
+static const t_fractol_pixel	g_colors[6] = {
+{1.0L, 0.0L, 0.0L},
+{1.0L, 1.0L, 0.0L},
+{0.0L, 1.0L, 0.0L},
+{0.0L, 1.0L, 1.0L},
+{0.0L, 0.0L, 1.0L},
+{1.0L, 0.0L, 1.0L},
+};
 
 static t_fractol_pixel	to_pixel(size_t size, size_t limit)
 {
-	t_fractol_pixel	result;
+	t_fractol_pixel		current;
+	t_fractol_pixel		next;
+	t_fractol_pixel		result;
+	const long double	alpha = 1.0L / limit * size;
 
-	result.r = 1.0L / (limit - 1) * size;
-	result.g = result.r;
-	result.b = result.r;
+	if (size == limit)
+		return (g_black);
+	current = g_colors[size / STEPS % 6];
+	current.r = 1.0L - ((1.0L - current.r) * alpha);
+	current.g = 1.0L - ((1.0L - current.g) * alpha);
+	current.b = 1.0L - ((1.0L - current.b) * alpha);
+	next = g_colors[(size / STEPS + 1) % 6];
+	next.r = 1.0L - ((1.0L - next.r) * alpha);
+	next.g = 1.0L - ((1.0L - next.g) * alpha);
+	next.b = 1.0L - ((1.0L - next.b) * alpha);
+	result.r = (current.r * (STEPS - size % STEPS)
+			+ next.r * (size % STEPS)) / STEPS;
+	result.g = (current.g * (STEPS - size % STEPS)
+			+ next.g * (size % STEPS)) / STEPS;
+	result.b = (current.b * (STEPS - size % STEPS)
+			+ next.b * (size % STEPS)) / STEPS;
 	return (result);
 }
 
