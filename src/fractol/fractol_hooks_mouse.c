58cf774ef57cfa340ec6264bb93afc697170f1e3
맹주영 <mjy9088@naver.com>
2022-09-01 23:47:38 +0900

    add correct zoom

--- src/fractol/fractol_hooks_mouse.c
+++ src/fractol/fractol_hooks_mouse.c
@@ -6,11 +6,33 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 22:50:46 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/09/01 22:53:29 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/09/01 23:45:13 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol_hooks.h"
+
+#include <stdio.h>
+
+static void	zoom(t_fractol *param, long double factor, int x, int y)
+{
+	const long double			new_distance_x
+		= (x - param->options->window_w / 2.0L) * factor;
+	const long double			new_distance_y
+		= (y - param->options->window_h / 2.0L) * factor;
+	const long double			clicked_x
+		= param->center.x + (x - param->options->window_w / 2.0L) * param->size;
+	const long double			clicked_y
+		= param->center.y + (y - param->options->window_h / 2.0L) * param->size;
+	const t_fractol_position	new_center = {
+		clicked_x - new_distance_x * param->size,
+		clicked_y - new_distance_y * param->size,
+	};
+
+	param->size *= factor;
+	param->center = new_center;
+	printf("current position: (%Lf, %Lf)\n", param->center.x, param->center.y);
+}
 
 int	fractol_hooks_mouse(int button, int x, int y, t_fractol *param)
 {
@@ -18,12 +40,12 @@ int	fractol_hooks_mouse(int button, int x, int y, t_fractol *param)
 	(void)y;
 	if (button == MLX_BUTTON_SCROLL_UP)
 	{
-		param->size *= 1.1;
+		zoom(param, 1.1L, x, y);
 		fractol_render(param);
 	}
 	else if (button == MLX_BUTTON_SCROLL_DOWN)
 	{
-		param->size /= 1.1;
+		zoom(param, 1.0L / 1.1L, x, y);
 		fractol_render(param);
 	}
 	else if (button == MLX_BUTTON_LEFT)
