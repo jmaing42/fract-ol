0a9ee2e97635bcb9cd80baeec13507b3ee59b60a
맹주영 <mjy9088@naver.com>
2022-09-02 00:52:35 +0900

    remove click hook

--- src/fractol/fractol_hooks_mouse.c
+++ src/fractol/fractol_hooks_mouse.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 22:50:46 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/09/01 23:52:53 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/09/02 00:52:28 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,14 +43,6 @@ int	fractol_hooks_mouse(int button, int x, int y, t_fractol *param)
 	else if (button == MLX_BUTTON_SCROLL_DOWN)
 	{
 		zoom(param, 1.0L / 1.1L, x, y);
-		fractol_render(param);
-	}
-	else if (button == MLX_BUTTON_LEFT)
-	{
-		param->center.x
-			+= (x - param->options->window_w / 2.0L) * param->size;
-		param->center.y
-			+= (y - param->options->window_h / 2.0L) * param->size;
 		fractol_render(param);
 	}
 	return (0);
